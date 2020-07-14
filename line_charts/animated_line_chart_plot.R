301d34debaa498583fd092b3d2147a291dea3749
Will Sutton <sutton_999@hotmail.com>
2020-07-14 12:55:57 +0100

    cleaning up animated line plot

--- line_charts/animated_line_chart_plot.R
+++ line_charts/animated_line_chart_plot.R
@@ -17,25 +17,29 @@ plot <- ggplot(df, aes(birth_year, percent_of_life)) +
   geom_segment(aes(xend = 0, yend = 1), linetype = 2, colour = '#720000') + 
   geom_point(size = 2,color = '#720000') + 
   guides(color = '#720000', fill = '#720000') +
-  labs(title = 'The Percentage of Your Life America Has Been At War, 1905-2019'
-       ,subtitle = 'If you were born in 1990 America has been at war for 67% of your life.'
-       ,x = 'Birth Year'
-       ,y = 'Percentage of Your Life The US Has Been At War'
-       ,caption = 'Author: @WJSutton12, Data: Washington Post') +
-  theme(plot.title = element_text(hjust = 0, size = 14),
-        plot.subtitle = element_text(hjust = 0, size = 10),
-        plot.caption = element_text(vjust = 0.3, size = 10),
-        axis.ticks.y = element_blank(),  
-        axis.ticks.x = element_blank(), 
-        axis.title.x = element_text(size = 12),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.line = element_line(colour = "black"),
-        plot.margin = margin(1,1,1,1, "cm")) + 
+  theme(
+    panel.grid.major = element_blank() # Remove gridlines (major)
+    ,panel.grid.minor = element_blank() # Remove gridlines (minor)
+    ,panel.background = element_blank() # Remove grey background
+    ,plot.title = element_text(hjust = 0, size = 20, colour = "#323232") # Title size and colour
+    ,plot.subtitle = element_text(hjust = 0, size = 12, colour = "#323232") # Subtitle size and colour
+    ,plot.caption = element_text(vjust = 0.3, size = 12, colour = "#323232") # Caption size and colour
+    ,axis.ticks.y = element_blank() # Remove tick marks (Y-Axis)
+    ,axis.text.y = element_text(hjust = 1, colour = "#323232", size = 12) # Axis size and colour (Y-Axis)
+    ,axis.title.y = element_text(size = 12, colour = "#323232") # Axis label size and colour (Y-Axis)
+    ,axis.ticks.x = element_blank() # Remove tick marks (X-Axis)
+    ,axis.text.x  = element_text(hjust = 1, colour = "#323232", size = 12) # Axis size and colour (X-Axis)
+    ,axis.title.x = element_text(size = 12, colour = "#323232") # Axis label size and colour (X-Axis)
+  ) +
+  labs(title = 'My awesome descriptive title' # Title text
+       ,subtitle = "Note: this is a subtitle" # Subtitle text
+       ,x = 'Value (units)' # X-Axis text 
+       ,y = 'Value (units)' # Y-Axis text 
+       ,caption = "Author: My Name, Source: ") +
   transition_reveal(birth_year)
 
 # Set animation
-anim <- animate(plot,200, duration = 10, width = 500, height = 400,start_pause=0, end_pause=30)
+anim <- animate(plot,200, duration = 10, width = 5, height = 4, units = 'in', res = 100, start_pause=0, end_pause=30)
 
 # Save animation
 anim_save("percentage_of_your_life_america_has_been_at_war.gif", anim)
